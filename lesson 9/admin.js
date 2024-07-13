2ea93661d4c49b05f935213c84047ddc627e45eb
HungTran181100 <hungtran00.nt@mindx.net.vn>
2024-07-13 19:44:07 +0700

    Update upload file img

--- lesson 9/admin.js
+++ lesson 9/admin.js
@@ -11,6 +11,13 @@ import {
   deleteDoc,
 } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
+
 const db = getFirestore(app);
 
 const postTitle = document.getElementById("postTitle");
@@ -25,6 +32,7 @@ onSnapshot(postQuery, (snapshot) => {
 
     output.innerHTML += `
         <h2>${post.title}</h2>
+        <img src="${post.image}" alt="">
         <p>${post.body}</p>
         <button onclick="deleteData('${doc.id}')">Delete</button>
         <button onclick="editData('${doc.id}')">Edit</button>
@@ -60,19 +68,30 @@ postTitle.addEventListener("submit", async (event) => {
 
   const title = document.getElementById("title").value;
   const body = document.getElementById("body").value;
+  const img = document.getElementById("img").files[0];
 
   if (title.length == 0 || body.length == 0) {
     console.log("Enter title or body");
   } else {
-    try {
-      const docRef = await addDoc(collection(db, "PXL-JSI05"), {
-        title: title,
-        body: body,
-      });
+    const storage = getStorage();
+    const imgPath = "Titles/" + new Date().valueOf();
+    const storageRef = ref(storage, imgPath);
 
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
+    // 'file' comes from the Blob or File API
+    uploadBytes(storageRef, img).then(async (snapshot) => {
+      console.log("Uploaded a blob or file!");
+      const url = await getDownloadURL(storageRef);
+      try {
+        const docRef = await addDoc(collection(db, "PXL-JSI05"), {
+          title: title,
+          body: body,
+          image: url,
+        });
+
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    });
   }
 });
